c23ac3ff5d0351a63bfe51256ce17ba0c2ef9237
Marlon Rodelo <rogofoodcanarias@gmail.com>
2026-03-19 18:03:58 +0000

    feat: módulo registro de accesos con exportación PDF

---
+++ custom-addons/gestori_access_log/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Gestori - Registro de Accesos",
+    "summary": "Log de accesos con exportación a PDF",
+    "version": "18.0.1.0.0",
+    "category": "Tools",
+    "author": "Gestori",
+    "license": "LGPL-3",
+    "depends": ["base", "web"],
+    "data": [
+        "security/ir.model.access.csv",
+        "report/access_log_report.xml",
+        "views/access_log_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
