2d389f75ccd1585baa24cb0ea65c0f2e90fa18ad
Marlon Rodelo <rogofoodcanarias@gmail.com>
2026-03-19 16:43:18 +0000

    fix: simplificar gestori_theme al mínimo para restaurar CSS

--- custom-addons/gestori_theme/__manifest__.py
+++ custom-addons/gestori_theme/__manifest__.py
@@ -1,23 +1,18 @@
 {
     "name": "Gestori Theme",
-    "summary": "Colores, logo y branding de Gestori",
+    "summary": "Colores y branding de Gestori",
     "version": "18.0.1.0.0",
     "category": "Theme",
     "author": "Gestori",
     "license": "LGPL-3",
     "depends": ["web"],
-    "data": [
-        "views/webclient_templates.xml",
-    ],
+    "data": [],
     "assets": {
         "web.assets_web": [
-            "gestori_theme/static/src/css/gestori_theme.css",
-        ],
-        "web.assets_frontend": [
             "gestori_theme/static/src/css/gestori_theme.css",
         ],
     },
     "installable": True,
     "application": False,
-    "auto_install": True,
+    "auto_install": False,
 }
